215c723fbdfac04c3289b04e13c7c73813575ad0
AmyQuin <149535311+AmyQuin@users.noreply.github.com>
2024-07-05 03:17:07 -0600

    06. Controlando formulario
    Lo que aprendimos en esta aula:
    
    -Trabajar con inputs
    -Que es el input controlado
    -Utilizar el evento onChange

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,11 +1,21 @@
+import { useState } from "react"
 import "./CampoTexto.css"
 
 const CampoTexto = (props) => {
-    console.log("Datos: ", props)
     const placeholderModificador=`${props.placeholder}...`
+
+    const manejarCambio = (e) => {
+        props.actualizarValor(e.target.value)
+    }
+
     return <div className="campo-texto">
         <label>{ props.titulo }</label>
-        <input placeholder={placeholderModificador} required={props.required}/>
+        <input 
+        placeholder={placeholderModificador} 
+        required={props.required} 
+        value={props.valor}
+        onChange={manejarCambio}
+        />
     </div>
 }
 //placeholder "es el texto que aparece atras antes de comenzar a escribir"
